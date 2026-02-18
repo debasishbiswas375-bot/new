e0a2d8df0eb925161f18dc2499ede7bdadae186e
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-18 14:34:00 +0530

    Update wsgi.py

--- admin/accountingtools/wsgi.py
+++ admin/accountingtools/wsgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from django.core.management import call_command
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'accountingtools.settings')
 
 application = get_wsgi_application()
 
