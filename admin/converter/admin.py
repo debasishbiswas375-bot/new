aa65859b0b5a731a7e458297f6b3ad8dbe412c8c
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-20 18:24:37 +0530

    Update admin.py

--- admin/converter/admin.py
+++ admin/converter/admin.py
@@ -4,19 +4,9 @@ from .models import Plan, UserProfile
 
 @admin.register(Plan)
 class PlanAdmin(admin.ModelAdmin):
-    list_display = (
-        'name',
-        'price',
-        'duration_months',
-        'credit_limit',
-    )
+    list_display = ('name', 'price', 'duration_months', 'credit_limit')
 
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = (
-        'user',
-        'plan',
-        'user_credits',
-        'expiry_date',
-    )
+    list_display = ('user', 'plan', 'user_credits', 'expiry_date')
