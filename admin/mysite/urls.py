028413a2339b54d346abcfe1c834f3f1c31cd24c
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-20 16:30:17 +0530

    Update urls.py

--- admin/mysite/urls.py
+++ admin/mysite/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.urls import path
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
-from core.views import register_user, login_user, home
+from converter.views import register_user, login_user, home
 
 
 # Auto-create admin user
