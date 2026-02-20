bea7111368c46748b0602516d8ddb0ad9348c209
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-20 18:31:06 +0530

    Update Packages.py

--- user/pages/Packages.py
+++ user/pages/Packages.py
@@ -4,11 +4,11 @@ def app():
     st.title("📦 Point Packages")
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.info("### Basic\n50 Points\n₹99")
+        st.info("### Basic\n50 Points/1 Months\n₹99")
         st.button("Buy Basic")
     with col2:
-        st.success("### Pro\n500 Points\n₹599")
+        st.success("### Pro\n500 Points/3 Months\n₹599")
         st.button("Buy Pro", type="primary")
     with col3:
-        st.info("### Enterprise\nUnlimited\n₹1999\12 Months")
+        st.info("### Enterprise\nUnlimited\n₹1999")
         st.button("Contact Us")
