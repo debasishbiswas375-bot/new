131905dd877dd6bb55c5dd2a4bf46226a50c4417
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-20 16:03:57 +0530

    Update urls.py

--- admin/mysite/urls.py
+++ admin/mysite/urls.py
@@ -1,23 +1,32 @@
 from django.contrib import admin
 from django.urls import path
-from django.shortcuts import redirect #
+from django.shortcuts import redirect
 from django.contrib.auth.models import User
+from .views import register_user, login_user
 
-# Keep your auto-admin trick
+
+# Auto-create admin
 def auto_create_admin():
     try:
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@example.com", "Deba9002043666")
+            User.objects.create_superuser(
+                "admin",
+                "admin@example.com",
+                "Deba9002043666"
+            )
     except Exception:
         pass
 
 auto_create_admin()
 
-# Function to handle the jump to Streamlit
+
 def jump_to_streamlit(request):
     return redirect("https://newtool.streamlit.app/")
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls), # Keep this so you can still use your admin panel
-    path('', jump_to_streamlit),     # This sends the home page to Streamlit
+    path('admin/', admin.site.urls),
+    path('api/register/', register_user),
+    path('api/login/', login_user),
+    path('', jump_to_streamlit),
 ]
