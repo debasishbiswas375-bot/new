d64afc726ebcba203edb8fa748c04b8e1ff5835b
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-20 16:52:06 +0530

    Update urls.py

--- admin/mysite/urls.py
+++ admin/mysite/urls.py
@@ -1,36 +1,12 @@
-import os
-print("FOLDERS:", os.listdir())
-
 from django.contrib import admin
 from django.urls import path
-from django.shortcuts import redirect
-from django.contrib.auth.models import User
-from converter.views import register_user, login_user, home
-
-
-# Auto-create admin user
-def auto_create_admin():
-    try:
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(
-                "admin",
-                "admin@example.com",
-                "Deba9002043666"
-            )
-    except Exception:
-        pass
-
-
-auto_create_admin()
-
-
-def jump_to_streamlit(request):
-    return redirect("https://newtool.streamlit.app/")
-
+from .views import register_user, login_user, user_info, convert_file
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/register/', register_user),
     path('api/login/', login_user),
-    path('', home),  # backend check
+    path('api/user-info/', user_info),
+    path('api/convert/', convert_file),
 ]
