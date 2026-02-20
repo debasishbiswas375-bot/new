94ba719ad7f510e64bddfab7d9a7ea4f0888b0b6
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-20 18:40:00 +0530

    Update urls.py

--- admin/mysite/urls.py
+++ admin/mysite/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from converter.views import register_user, login_user
+from converter.views import register_user, login_user, get_plans
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +8,7 @@ urlpatterns = [
     # API endpoints
     path('register/', register_user),
     path('login/', login_user),
+
+    # NEW
+    path('plans/', get_plans),
 ]
