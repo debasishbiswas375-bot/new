9e75e3cc02aff336a09be572edeba395754f147c
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-21 20:57:21 +0530

    Update create_admin.py

--- admin/create_admin.py
+++ admin/create_admin.py
@@ -1,20 +1,11 @@
 import os
 import django
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 django.setup()
-
-from django.contrib.auth.models import User
-
-username = 'deba'
-email = 'admin@example.com'
-password = 'Deba@9002'
-
-if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print(f"Superuser {username} created successfully!")
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+    print("Admin created!")
 else:
-    user = User.objects.get(username=username)
-    user.set_password(password)
-    user.save()
-    print(f"Password for {username} has been updated!")
+    print("Admin already exists.")
