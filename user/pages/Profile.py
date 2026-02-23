619442ba188973a5cef345e3813938040908815a
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-23 12:01:24 +0530

    Delete user directory

--- user/pages/Profile.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def app():
-    st.title("👤 My Profile")
-    if "username" in st.session_state:
-        st.write(f"**Username:** {st.session_state['username']}")
-        st.write(f"**Current Points:** {st.session_state.get('credits', 100)}")
-    else:
-        st.warning("Please log in to view your profile details.")
