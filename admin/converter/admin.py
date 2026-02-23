2bc1bf6b28798ad6a5fed7bc3f17bd24ae0e3a43
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-23 18:05:27 +0530

    Update admin.py

--- admin/converter/admin.py
+++ admin/converter/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Plan, UserProfile
 
-admin.site.register(Plan)
-admin.site.register(UserProfile)
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ("name", "amount", "credit", "month")
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "plan", "expiry_date")
