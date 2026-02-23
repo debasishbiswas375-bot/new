fa3e5336cdcfab149fe4767ff026eaadcdb1e004
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-23 11:53:13 +0530

    Delete admin directory

--- admin/mysite/wsgi.py
+++
@@ -1,8 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# Set the settings module for your project
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
-# This is the only line needed to start the web server
-application = get_wsgi_application()
