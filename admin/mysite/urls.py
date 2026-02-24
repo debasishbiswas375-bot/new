048306129545526b9fa91c9584abb290513395cd
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-24 17:32:57 +0530

    Update urls.py

--- admin/mysite/urls.py
+++ admin/mysite/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect
+
+def home_redirect(request):
+    return redirect("https://newtool.streamlit.app")
 
 urlpatterns = [
+    path("", home_redirect),  # root redirect
     path("admin/", admin.site.urls),
 ]
