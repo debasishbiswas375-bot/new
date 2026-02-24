28c740278d33583443d638de9f9526b9e3d9b6b8
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-24 15:56:02 +0530

    Update signals.py

--- admin/converter/signals.py
+++ admin/converter/signals.py
@@ -3,7 +3,12 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import UserProfile
 
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Create profile ONLY when user is first created.
+    This prevents infinite loop and 502 error during delete.
+    """
     if created:
         UserProfile.objects.create(user=instance)
