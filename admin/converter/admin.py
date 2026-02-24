7d9b3f9acf0f4c05ddb99fc42a2c5f74f58e2cf9
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-24 16:09:44 +0530

    Update admin.py

--- admin/converter/admin.py
+++ admin/converter/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
 from .models import UserProfile, Plan
+from django.contrib.auth.models import User
 
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ("id", "safe_user", "plan", "user_credits", "expiry_date")
-    search_fields = ("user__username", "full_name")
+    search_fields = ("full_name",)
+
+    def get_queryset(self, request):
+        """
+        Only show profiles that still have a valid user.
+        This prevents 500 error.
+        """
+        qs = super().get_queryset(request)
+        return qs.filter(user__isnull=False)
 
     def safe_user(self, obj):
-        if obj.user_id:
-            return obj.user.username
-        return "Deleted User"
+        return obj.user.username if obj.user_id else "Deleted User"
 
     safe_user.short_description = "User"
 
