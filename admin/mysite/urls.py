48a3ad56ca04999d9064091d1a99d9c93c2bf7d5
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-24 16:34:54 +0530

    Update urls.py

--- admin/mysite/urls.py
+++ admin/mysite/urls.py
@@ -1,26 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from django.shortcuts import redirect
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    # LOGIN
-    path(
-        "login/",
-        auth_views.LoginView.as_view(
-            template_name="login.html",
-            redirect_authenticated_user=True
-        ),
-        name="login"
-    ),
-
-    # LOGOUT (secure POST version)
-    path(
-        "logout/",
-        auth_views.LogoutView.as_view(next_page="/"),
-        name="logout"
-    ),
 ]
