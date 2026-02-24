6473ac29a56cbc26a86bd83ae991062bdde35ae3
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-24 16:08:50 +0530

    Update admin.py

--- admin/converter/admin.py
+++ admin/converter/admin.py
@@ -4,8 +4,15 @@ from .models import UserProfile, Plan
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "plan", "user_credits", "expiry_date")
+    list_display = ("id", "safe_user", "plan", "user_credits", "expiry_date")
     search_fields = ("user__username", "full_name")
+
+    def safe_user(self, obj):
+        if obj.user_id:
+            return obj.user.username
+        return "Deleted User"
+
+    safe_user.short_description = "User"
 
 
 @admin.register(Plan)
