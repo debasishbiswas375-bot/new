e3cf56d825031b88094fec87900a374667a08dcd
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-24 15:57:18 +0530

    Update admin.py

--- admin/converter/admin.py
+++ admin/converter/admin.py
@@ -1,21 +1,13 @@
 from django.contrib import admin
-from .models import Plan, UserProfile
+from .models import UserProfile, Plan
 
-@admin.register(Plan)
-class PlanAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'duration_months', 'credit_limit')
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    # Use list_select_related to reduce database hits to Supabase
-    list_select_related = ('user', 'plan')
-    list_display = ('get_username', 'get_plan_name', 'user_credits', 'expiry_date')
-    search_fields = ('user__username', 'full_name', 'phone')
+    list_display = ("user", "plan", "user_credits", "expiry_date")
+    search_fields = ("user__username", "full_name")
 
-    def get_username(self, obj):
-        return obj.user.username if obj.user else "No User"
-    get_username.short_description = 'Username'
 
-    def get_plan_name(self, obj):
-        return obj.plan.name if obj.plan else "No Plan"
-    get_plan_name.short_description = 'Plan'
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "credit_limit", "duration_months")
