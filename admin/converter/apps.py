aab37a9f00f49930f57ae864eca1a8215d99633a
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-24 14:21:43 +0530

    Update apps.py

--- admin/converter/apps.py
+++ admin/converter/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 class ConverterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'converter'
+
+    def ready(self):
+        import converter.signals
