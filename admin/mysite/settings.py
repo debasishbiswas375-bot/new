d1152e1772a8663e2d8348ce5fa0c4e24cc02478
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-25 16:10:34 +0530

    Update settings.py

--- admin/mysite/settings.py
+++ admin/mysite/settings.py
@@ -5,6 +5,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 DEBUG = False
 
