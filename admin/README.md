4e2db6cbdc3e56d1f2662802b6c04142a6913c80
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-25 13:27:28 +0530

    Delete admin directory

--- admin/README.md
+++
@@ -1,5 +0,0 @@
----
-title: My Solara App
-sdk: docker
-app_port: 7860
----
--- admin/converter/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ConverterConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "converter"
-
-    def ready(self):
-        import converter.signals
