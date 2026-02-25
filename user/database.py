58449f02e94f9787fea85ff5cc1981d4bf4014c6
Debasish Biswas <debasish.biswas375@gmail.com>
2026-02-25 15:49:55 +0530

    Delete user directory

--- user/database.py
+++
@@ -1,7 +0,0 @@
-import psycopg2
-import streamlit as st
-
-def get_db_connection():
-    """Establishes connection to the Supabase PostgreSQL database."""
-    # This expects a secret named DATABASE_URL in your Streamlit dashboard
-    return psycopg2.connect(st.secrets["DATABASE_URL"])
